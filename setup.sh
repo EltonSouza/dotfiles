daed752ff895b636e7ea79b662e2bedbea7f881a
Elton Souza <elton.souza@gmail.com>
2018-03-17 16:59:41 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -190,7 +190,7 @@ printf "\n\ninstalling vscode\n"
 if ! command_exists code; then
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-    sudo dnf install code
+    sudo dnf install -y code
 else
     echo "vscode is already installed"
 fi
